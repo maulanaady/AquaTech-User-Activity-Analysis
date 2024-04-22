368fb2c39ad695760a685fa1fffd7e8a6a11b61d
adysulton <adysulton@gmail.com>
2024-04-22 10:02:08 +0700

    change raw_data link

--- download_raw_data.sh
+++ download_raw_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget --no-check-certificate \
-  'https://drive.usercontent.google.com/download?id=1Ip58phB-709LIdX2l3TeAonKXK1Qg9q7&export=download&confirm=t&uuid=cbd2ee2e-3ab3-40c5-b001-ba5e027026aa' \
+  'https://drive.usercontent.google.com/download?id=1CX8Gne2tdAqpOXBCeIug6ADxRpyVW9oV&export=download&confirm=t&uuid=cbd2ee2e-3ab3-40c5-b001-ba5e027026aa' \
   -O raw_data.zip

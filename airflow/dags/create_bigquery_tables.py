9971dab06acfbcaba456e7545c18c69606e47fe9
adysulton <adysulton@gmail.com>
2024-04-24 16:13:18 +0700

    create dag to automate Bigquery table creation

---
+++ airflow/dags/create_bigquery_tables.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+from datetime import datetime
+
+dag = DAG('Create Bigquery tables',
+          description='A DAG to execute SQL files from a specific path',
+          start_date=datetime(2024, 4, 24),
+          template_searchpath=["/opt/airflow/data/"],
+          schedule='@once'
+        )
+
+
+execute_query = BigQueryExecuteQueryOperator(
+    task_id='execute_query',
+    gcp_conn_id='google_client',
+    sql='ddl_table.sql',
+    use_legacy_sql=False,
+    dag=dag
+)
+
+
+if __name__ == '__main__':
+    dag.test()
